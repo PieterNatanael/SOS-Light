9179c7df1abb48e83ecd2deba682ae02b9a6b93f
PieterNatanael <pieternatanael@icloud.com>
2024-06-11 10:56:09 +0800

    Initial Commit

---
+++ SOS Light/SOS_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOS_LightApp.swift
+//  SOS Light
+//
+//  Created by Pieter Yoshua Natanael on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SOS_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
